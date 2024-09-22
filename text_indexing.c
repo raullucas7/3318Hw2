bd3cb34ee60e216d9371bd11426af76d2a6d5c4f
raullucas7 <139395594+raullucas7@users.noreply.github.com>
2024-09-21 20:21:11 -0500

    added starter code

--- text_indexing.c
+++ text_indexing.c
@@ -15,10 +15,22 @@
 
 
 
-int main () 
+int main(int argc, char** argv) 
 {
-    char filename_input;
-    int mode_input = 0;
-
+    char *fname = NULL;
+    int mode = 0; // 0 - normal, 1 - verbose
     
+    if (argc < 3 ) 
+    {
+        printf("Not enough arguments. Need mode and filename, e.g.: 0 caps.txt\n");
+        return 0;
+    }
+    else
+    {
+        fname = argv[2];
+        mode = atoi(argv[1]);
+    }
+    
+    printf("mode: %d file: %s\n", mode, fname); // KEEP THIS LINE
+    return 0;
 }
